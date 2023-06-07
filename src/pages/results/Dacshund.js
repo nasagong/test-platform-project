65f1b3b56d2eec5d61d4ee69835e83a7219de797
nasagong <nasagong@naver.com>
2023-06-07 14:53:07 +0000

    06-07 share test

--- src/pages/results/Dacshund.js
+++ src/pages/results/Dacshund.js
@@ -3,7 +3,19 @@ import * as Styled from "../../styles/Results.styled";
 import { useState, useEffect } from "react";
 import Loading from "../Loading";
 
+
 const Dachshund = () => {
+  const handle = () => {
+    if (navigator.share) {
+        navigator.share({
+            title: '기록하며 성장하기',
+            text: 'Hello World',
+            url: 'https://shinsangeun.github.io',
+        });
+    }else{
+        alert("공유하기가 지원되지 않는 환경 입니다.")
+    }
+  }
   useEffect(() => {
     const timeout = setTimeout(() => {
       setLoadingFinished(true);
@@ -34,6 +46,8 @@ const Dachshund = () => {
           </li>
         </ul>
       </Styled.Description>
+      <button onClick={()=>handle()}>share share share</button>
+      <button > Back to HOME </button>
     </Styled.Container>
   );
 };
